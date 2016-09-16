7df4645b81bbf11a534c042fab886ec8ce050842
firepunch <cynne13@naver.com>
2016-09-16 14:22:43 +0900

    server.js initial state

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var
     http = require('http'),
     fs = require('fs'),
-    path = require('path'),
 
     express = require('express'),
     app = express(),
     server = http.createServer(app),
     io = require('socket.io').listen(server),
-    players = [],
 
     PORT = 3002;
 
@@ -20,37 +18,27 @@ app.get('/', function (req, res) {
     });
 });
 
-io.on('connection', function(socket){
+
+io.sockets.on('connection', function (socket) {
     console.log("Client " + socket.id + " connected");
-	socket.emit('socketID', { id: socket.id });
-	socket.emit('getPlayers', players);
-	socket.broadcast.emit('newPlayer', { id: socket.id });
+
+    socket.emit('message', 'Login success !');
+    socket.broadcast.emit('message', 'Another socket login success! ');
+
     socket.on('nickname', function (nickname) {
         socket.nickname = nickname;
     });
+
     socket.on('message', function (message) {
         console.log(socket.nickname + ' --- ' + socket.id + ' to server : ' + message);
-    });    
-	socket.on('disconnect', function(){
-        console.log("Client " + socket.id + " disconnected");
-		socket.broadcast.emit('playerDisconnected', { id: socket.id });
-		for(var i = 0; i < players.length; i++){
-			if(players[i].id == socket.id){
-				players.splice(i, 1);
-			}
-		}
-	});
-	players.push(new player(socket.id, 0, 0));
+    });
+
+    return socket.on("disconnect", function() {
+        return console.log("Client " + socket.id + " disconnected");
+    });
 });
 
-// io.sockets.on('connection', function (socket) {
 
 server.listen(port = Number(process.env.PORT || PORT), function() {
-    console.log("Server "+ PORT +" listening");
+    console.log("Server "+PORT+" listening");
 });
-
-function player(id, x, y) {
-    this.id = id;
-    this.x = x;
-    this.y = y;
-}
