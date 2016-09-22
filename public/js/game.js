1d3f44fb1779176ab1293d1388de1aa8fb9721a5
firepunch <cynne13@naver.com>
2016-09-22 10:17:48 +0900

    new map,player / not move

---
+++ public/js/game.js
@@ -0,0 +1,19 @@
+class spriteSheet{
+    constructor(img, numOfX, numOfY, w,h){
+        this.img = img;
+        this.numOfX = numOfX;
+        this.numOfY = numOfY;
+        this.width = w;
+        this.height = h;
+    }
+
+    getSheet(index){
+        let canvas = document.createElement('canvas');
+        let ctx = canvas.getContext('2d');
+        canvas.width = this.width;
+        canvas.height = this.height;
+
+        ctx.drawImage(this.img, this.width * (index % this.numOfX), this.height * Math.floor(index / this.numOfX), this.width, this.height, 0, 0, canvas.width, canvas.height);
+        return canvas;
+    }
+}
