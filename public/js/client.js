97d4d9f3f20152821adcd65693273325a875b626
firepunch <cynne13@naver.com>
2016-09-22 10:26:01 +0900

    seperate socket and player

--- public/js/client.js
+++ public/js/client.js
@@ -52,6 +52,6 @@ socket.on('newPosition',function(data){
     ctx.drawImage(Img.map, 0, 0, 1340, 640, 0, 0, canvas.width, canvas.height);
 
     for (var i=0; i<data.length; i++){
-        ctx.drawImage(skeletonSheet.getSheet(0));
+        ctx.drawImage(skeletonSheet.getSheet(0), data[i].x, data[i].y);
     }
 });
--- server.js
+++ server.js
@@ -17,29 +17,41 @@ server.listen(port = Number(process.env.PORT || PORT), function(){
 });
 
 var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
+var Player = function (id){
+	var self = {
+		x:250,
+		y:250,
+		id:id,
+		number:""+Math.floor(10*Math.random()) //왱
+	}
+	return self;
+}
 
 io.sockets.on('connection', function(socket){
 	socket.id = Math.random();
-	socket.x = 0;
-	socket.y = 0;
-	socket.number = ""+Math.floor(10*Math.random()); //왱
 	SOCKET_LIST[socket.id] = socket;
+
+	var player = Player(socket.id);
+	PLAYER_LIST[socket.id] = player;
 
 	socket.on('disconnect', function(){
 		delete SOCKET_LIST[socket.id];
+		delete PLAYER_LIST[socket.id];
 	})
 });
 
 setInterval(function() {
 	var pack = [];
-	for (var i in SOCKET_LIST) {
-		var socket = SOCKET_LIST[i];
-		socket.x++;
-		socket.y++;
+	for (var i in PLAYER_LIST) {
+		var player = PLAYER_LIST[i];
+		player.x++;
+		player.y++;
 		pack.push({
-			x: socket.x,
-			y: socket.y,
-			number:socket.number
+			x: player.x,
+			y: player.y,
+			number:player.number
 		});
 	}
 	for (var i in SOCKET_LIST) {
